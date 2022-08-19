994952c55f33ae69e52be3e22f02a738620dbf44
Ousama Alnemer <ousama.alnemer@nordcloud.com>
2022-08-19 07:36:37 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,14 @@
 */
 
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+  required_version = ">= 0.14.9" 
+    
   backend "azurerm" {
     resource_group_name  = "terraform-rg"
     storage_account_name = "abcd1234abcd"
