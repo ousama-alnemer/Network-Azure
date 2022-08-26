1e87d98e4e48feba0425dabdca0cf0be02a9ec03
Ousama Alnemer <ousama.alnemer@nordcloud.com>
2022-08-26 15:42:04 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -159,7 +159,7 @@ variable "publicipid" {
   description = "The id of the public ip address."
 }
 
-variable "countnr" {
+variable "vm_count" {
 type = number
  description = "The number of servers." 
 }
