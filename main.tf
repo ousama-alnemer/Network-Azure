0a737216030d0177717fcf4daa8d7c5b0af8c8cb
Ousama Alnemer <ousama.alnemer@nordcloud.com>
2022-08-26 12:33:58 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -123,6 +123,6 @@ module "virtualmachine-windows" {
   vmimageversion             = var.vmimageversion
   vmosdiskcaching            = var.vmosdiskcaching
   vmosdiskstorageaccounttype = var.vmosdiskstorageaccounttype
-  node_count                 = 2
+  node                       = 2
 
 }
